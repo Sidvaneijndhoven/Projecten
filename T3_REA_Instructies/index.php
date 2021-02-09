fbc4d045297174b48ea9e2f5d40f34902e45705c
everskw1c <r.evers@kw1c.nl>
2021-02-09 20:20:34 +0100

    Update map structuur

---
+++ T3_REA_Instructies/index.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * User: S. Tudent
+ * Date: Date
+ * File: index.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>
+            <?php
+                echo 'Realiseren instructies thema 3 en 4.';
+            ?>
+        </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="styles/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1>
+                <?php
+                    echo 'Instructies';
+                ?>
+            </h1>
+        </header>
+        <?php
+            //Hier gaan we in het volgende hoofdstuk het menu toevoegen.
+        ?>
+        <main id="wrapper">
+            <h2>
+                Uitwerkingen lesinstructies.
+            </h2>
+            <p>
+                <?php
+                    echo "Hello World!";
+                ?>
+            </p>
+        </main>
+    </body>
+</html>
+
