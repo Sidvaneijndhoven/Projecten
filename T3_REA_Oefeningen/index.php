4c57193d8beaf407bf901fec7c48f8ea5b41c244
ron@r-spierings.nl <ron@r-spierings.nl>
2021-03-09 14:14:21 +0100

    Klaarzetten index.php

--- T3_REA_Oefeningen/index.php
+++ T3_REA_Oefeningen/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * User: S. Tudent
- * Date: Date
+ * User: R. Spierings
+ * Date: 09-03-2021
  * File: index.php
  */
 ?>
@@ -33,12 +33,13 @@
                     </ul>
                     <li>Hoofdstuk 3</li>
                     <ul>
-                        <li><a href="/T3_REA_Oefeningen/hoofdstuk2/T3_REA_Oefening_3_1.php">Oefening 3.1</a></li>
-                        <li><a href="/T3_REA_Oefeningen/hoofdstuk2/T3_REA_Oefening_3_2.php">Oefening 3.2</a></li>
+                        <li><a href="/T3_REA_Oefeningen/hoofdstuk3/T3_REA_Oefening_3_1.php">Oefening 3.1</a></li>
+                        <li><a href="/T3_REA_Oefeningen/hoofdstuk3/T3_REA_Oefening_3_2.php">Oefening 3.2</a></li>
                     </ul>
                     <li>Hoofdstuk 4</li>
                     <ul>
-
+                        <li><a href="/T3_REA_Oefeningen/hoofdstuk4/T3_REA_Oefening_4_1.php">Oefening 4.1</a></li>
+                        <li><a href="/T3_REA_Oefeningen/hoofdstuk4/T3_REA_Oefening_4_2.php">Oefening 4.2</a></li>
                     </ul>
                 </ul>
             </nav>
@@ -56,6 +57,9 @@
                 </p>
             </main>
         </div>
+        <footer>
+            <p>&copy; Ron Spierings | IO1E4 | 09-03-2021 </p>
+        </footer>
     </body>
 </html>
 
