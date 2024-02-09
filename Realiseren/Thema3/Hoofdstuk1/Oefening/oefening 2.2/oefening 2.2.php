413016462c7a1e71a021953ffff2a1d09da018c2
SidvanEijndhoven <sidvaneijndhoven@edu-kw1c.nl>
2024-02-09 15:26:59 +0100

    T3_REA_Oefening_2_2

---
+++ Realiseren/Thema3/Hoofdstuk1/Oefening/oefening 2.2/oefening 2.2.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * User: sid van eijndhoven
+ * Date: 5-2-2024
+ * File: T3_REA_Oefening 2.1.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../oefening%202.2/styles/oefening%202.2.css" rel="stylesheet">
+</head>
+<body>
+<header>
+</header>
+<main>
+    <section>
+        <?php
+            echo "<h1>Uitwerking oefening 2.1</h1>"
+        ?>
+        <?php
+        echo "<h1> 
+                    Nieuwe Israëlische luchtaanvallen op Rafah vergroten zorgen over grondoffensief
+               </h1>"
+        ?>
+        <?php
+        echo "<h2 id='h2'>
+                 Israël heeft vrijdagochtend opnieuw luchtaanvallen uitgevoerd op de stad Rafah, in het zuiden van de Gazastrook. Daarbij zijn minstens acht mensen omgekomen. De luchtaanvallen vergroten de zorgen over een mogelijk grondoffensief in de stad.
+              </h2>"
+        ?>
+        <?php
+            echo "<p>
+                        Bij de luchtaanvallen op Rafah werden twee woningen geraakt. Ten minste acht mensen kwamen daarbij om het leven.
+
+                        Een dag eerder werd ook al melding gemaakt van twee luchtaanvallen op het vluchtelingenkamp Tel Al Sultan in Rafah. Daarbij kwamen zeker dertien mensen om het leven.
+
+                        Meer dan de helft van de 2,3 miljoen inwoners van Gaza verblijft op dit moment in Rafah, dat dicht bij de grens met Egypte ligt. Voor velen is het de laatste veilige plek in de Gazastrook. De nieuwe aanvallen op Rafah zorgen dan ook voor veel paniek onder de gevluchte Palestijnen die daar verblijven.
+                </p>"
+        ?>
+        <?php
+            echo "<img src='../oefening%202.2/images/OIP.jpg'>"
+        ?>
+        <?php
+            echo "<h1>Nieuwe Israëlische luchtaanvallen op Rafah vergroten zorgen over grondoffensief</h1><h2>Israël heeft vrijdagochtend opnieuw luchtaanvallen uitgevoerd op de stad Rafah, in het zuiden van de Gazastrook. Daarbij zijn minstens acht mensen omgekomen. De luchtaanvallen vergroten de zorgen over een mogelijk grondoffensief in de stad.</h2><p>Bij de luchtaanvallen op Rafah werden twee woningen geraakt. Ten minste acht mensen kwamen daarbij om het leven.
+
+                        Een dag eerder werd ook al melding gemaakt van twee luchtaanvallen op het vluchtelingenkamp Tel Al Sultan in Rafah. Daarbij kwamen zeker dertien mensen om het leven.
+
+                        Meer dan de helft van de 2,3 miljoen inwoners van Gaza verblijft op dit moment in Rafah, dat dicht bij de grens met Egypte ligt. Voor velen is het de laatste veilige plek in de Gazastrook. De nieuwe aanvallen op Rafah zorgen dan ook voor veel paniek onder de gevluchte Palestijnen die daar verblijven.</p><img src='images/OIP.jpg'>"
+        ?>
+    </section>
+</main>
+</body>
+</html>
+
+
+
