e9b1d916fdac92f9f4125256354e6fe5d0361174
SidvanEijndhoven <sidvaneijndhoven@edu-kw1c.nl>
2024-02-19 12:23:14 +0100

    T3_REA_Oefening_3_1

--- Realiseren/Thema3/Template/template.php
+++ Realiseren/Thema3/Template/template.php
@@ -17,6 +17,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="styles/style.css" rel="stylesheet">
     </head>
+
+
     <body>
         <header>
             <h1>
