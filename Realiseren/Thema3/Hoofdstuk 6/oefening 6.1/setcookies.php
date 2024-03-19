a39b17360200d154d983f9036e52743d01a85e39
SidvanEijndhoven <sidvaneijndhoven@edu-kw1c.nl>
2024-03-19 09:19:32 +0100

    test

---
+++ Realiseren/Thema3/Hoofdstuk 6/oefening 6.1/setcookies.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <title>Oefening</title>
+</head>
+<body>
+<?php
+    $expirataionDate = time() + 3600 * 24 * 14;
+    setcookie("userCountCookie", 1, $expirataionDate, "/" )
+?>
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
